5b9f3cba824267629f1c5c374bc56ad3e4a4ab0a
Ztrolix <xdpxigaming@gmail.com>
2024-06-24 12:11:24 +1000

    Fixed All Times w/ Time Calc

--- timeCalculator.py
+++ timeCalculator.py
@@ -1,9 +1,20 @@
 from datetime import datetime
 
-start_date = datetime(1970, 1, 1, 11, 0, 0)
-end_date = datetime(2024, 6, 16, 11, 0, 0)
-time_difference = end_date - start_date
-milliseconds_difference = time_difference.total_seconds() * 1000
-milliseconds_difference
+print("------------------------")
 
-print(milliseconds_difference)
+while True:
+
+	start_date = datetime(1970, 1, 1, 11, 0, 0)
+	year = input("Year: ")
+	month = input("Month: ")
+	day = input("Day: ")
+	end_date = datetime(int(year), int(month), int(day), 11, 0, 0)
+	time_difference = end_date - start_date
+	milliseconds_difference = time_difference.total_seconds() * 1000
+	milliseconds_difference
+
+	print("------------------------")
+
+	print(milliseconds_difference)
+
+	print("------------------------")
