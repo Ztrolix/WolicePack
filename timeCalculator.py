72e166592e9c1347d46c40ae408b05a115fd273a
Ztrolix <xdpxigaming@gmail.com>
2024-06-24 12:32:32 +1000

    Changed Formatting of Code

--- timeCalculator.py
+++ timeCalculator.py
@@ -1,20 +1,18 @@
 from datetime import datetime
 
 print("------------------------")
-
 while True:
-
 	start_date = datetime(1970, 1, 1, 11, 0, 0)
+	
 	year = input("Year: ")
 	month = input("Month: ")
 	day = input("Day: ")
+
 	end_date = datetime(int(year), int(month), int(day), 11, 0, 0)
 	time_difference = end_date - start_date
 	milliseconds_difference = time_difference.total_seconds() * 1000
 	milliseconds_difference
 
 	print("------------------------")
-
 	print(milliseconds_difference)
-
 	print("------------------------")
